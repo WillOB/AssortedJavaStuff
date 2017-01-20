144d3da794ff9bd555f1dff8be39153a863d15b8
WillOB <obrienwp@s.dcsdk12.org>
2017-01-19 22:08:23 -0700

    Initial commit

---
+++ src/sort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class sort {
+
+	public static void main(String[] args) {
+		int length = 5;
+		
+		public int[] createDeck() {
+			int[] deck = new int[ length ];
+			int count;
+			Random rand = new Random();
+			for (count = 0; count < length; count++) {
+				deck[count] = rand.nextInt(14);
+			}
+			return deck;
+		}
+		
+		int i;
+		for (i = 0; i < length; i++) {
+			
+		}
+		
+	}
+
+}
