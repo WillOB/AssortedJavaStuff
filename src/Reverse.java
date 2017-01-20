5518cadd1ccf463aaf5a7bfd7f624ee41f7d86d6
William Patrick O Brien <obrienwp@dcsdk12.org>
2017-01-20 12:55:39 -0700

    Add reverse and shuffle

---
+++ src/Reverse.java
@@ -0,0 +1,14 @@
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		int[] deck = new int[5];
+		int length = deck.length;
+		int i;
+		for (i = 0; i < length; i++) {
+		deck[0] = deck[length - 1];
+		}
+
+	}
+
+}
---
+++ src/shuffle.java
@@ -0,0 +1,10 @@
+
+public class shuffle {
+
+	public static void main(String[] args) {
+		int[] deck = new int[5];
+		deck 
+
+	}
+
+}
